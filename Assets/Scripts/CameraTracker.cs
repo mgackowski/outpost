57a243b46066fb400b3f4fe5f059488a8dba435d
mgackowski <mwgackowski@gmail.com>
2020-11-07 18:59:10 +0000

    Initial commit

---
+++ Assets/Scripts/CameraTracker.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+
+public class CameraTracker : MonoBehaviour
+
+{
+    public Transform target;
+    public float smoothTime;
+    public float targetZPosition = -10f;
+
+    private Vector3 interpolatedPosition;
+    private Vector3 cameraVelocity = Vector3.zero;
+
+
+    // LateUpdate is called after all other objects have moved
+    void LateUpdate()
+    {
+        if (target != null)
+        {
+            interpolatedPosition = Vector3.SmoothDamp(transform.position, target.position, ref cameraVelocity, smoothTime);
+            interpolatedPosition.z = targetZPosition;
+            transform.position = interpolatedPosition;
+        }
+    }
+
+}
---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+
+public class PlayerControls : MonoBehaviour
+{
+    private float moveHorizontal;
+    private float moveVertical;
+    private Vector2 movement;
+
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //GetAxis() produces smoothing when played with keyboard
+        moveHorizontal = Input.GetAxisRaw("Horizontal");
+        moveVertical = Input.GetAxisRaw("Vertical");
+        movement = new Vector2(moveHorizontal, moveVertical);
+        if (movement.magnitude > 1) movement = movement.normalized;
+
+    }
+
+    void FixedUpdate()
+    {
+        GetComponent<Rigidbody2D>().velocity = movement * speed;
+    }
+}
