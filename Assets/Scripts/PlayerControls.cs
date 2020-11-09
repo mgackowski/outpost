f49045f0f69845a28545aef32b2fbb606acd7e99
mgackowski <mwgackowski@gmail.com>
2020-11-09 21:08:58 +0000

    Player has orientation and area trigger

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 [RequireComponent(typeof(Rigidbody2D))]
 
@@ -9,23 +10,31 @@ public class PlayerControls : MonoBehaviour
     private float moveHorizontal;
     private float moveVertical;
     private Vector2 movement;
+    private Text debugText;
 
     public float speed;
+    public float orientation = 180;
+    public CircleCollider2D interactionArea;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        debugText = GameObject.Find("DebugText").GetComponent<Text>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //GetAxis() produces smoothing when played with keyboard
+
         moveHorizontal = Input.GetAxisRaw("Horizontal");
         moveVertical = Input.GetAxisRaw("Vertical");
         movement = new Vector2(moveHorizontal, moveVertical);
-        if (movement.magnitude > 1) movement = movement.normalized;
+        if (movement.magnitude > 1) movement = movement.normalized; //slower diagonal mvmt on keyboard
+
+        if (movement.magnitude > 0) orientation = Vector2.SignedAngle(Vector2.up, movement);
+
+        interactionArea.offset = Quaternion.Euler(0, 0, orientation) * Vector2.up;
+        debugText.text = orientation.ToString();
 
     }
 
@@ -33,4 +42,6 @@ public class PlayerControls : MonoBehaviour
     {
         GetComponent<Rigidbody2D>().velocity = movement * speed;
     }
+
+
 }
