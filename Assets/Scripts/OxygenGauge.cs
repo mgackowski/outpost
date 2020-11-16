968ebc12d1baed5c45c5f2c79c27cfc49245b060
mgackowski <mwgackowski@gmail.com>
2020-11-16 21:08:41 +0000

    Recreated fade

---
+++ Assets/Scripts/OxygenGauge.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OxygenGauge : MonoBehaviour
+{
+    private RectTransform rectTransform;
+    public float maxOxygenLevel = 600f;
+
+    void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void setDisplayedValue(float oxygenLevel)
+    {
+        float zeroOxygenValue = 86f;
+        float fullOxygenValue = -70f;
+        float percentage = oxygenLevel / maxOxygenLevel;
+        float targetMaxRange = fullOxygenValue - zeroOxygenValue;
+        float finalAngle = zeroOxygenValue + (percentage * targetMaxRange);
+
+        rectTransform.rotation = Quaternion.Euler(0, 0, finalAngle);
+    }
+}
